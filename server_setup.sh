0df96956e4fa02d8c37f0f1486ac128fdbb197b0
Shreyansh Das <shreyanshdas00@gmail.com>
2020-11-23 14:00:08 +0530

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -3,7 +3,7 @@ sudo apt install g++ build-essential git curl python-minimal
 curl https://install.meteor.com/ | sh
 meteor npm install -g @rocket.chat/apps-cli
 meteor rc-apps create
-cp -v App.ts /direct-contact/
+cp -v App.ts direct-contact/
 cd direct-contact
 sudo rm DirectContactApp.ts
 mv App.ts DirectContactApp.ts
