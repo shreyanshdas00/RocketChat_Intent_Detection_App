36b3ad5252aef44d0663ffc31185b1549bb8fa8e
Shreyansh Das <shreyanshdas00@gmail.com>
2020-11-23 14:09:53 +0530

    Create server_setup.sh

---
+++ server_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt install g++ build-essential git curl python-minimal
+curl https://install.meteor.com/ | sh
+meteor npm install -g @rocket.chat/apps-cli
+meteor rc-apps create
+cp -v App.ts direct-contact/
+cd direct-contact
+sudo rm DirectContactApp.ts
+mv App.ts DirectContactApp.ts
+cd ..
+cd rocket.chat
+docker-compose up -d
