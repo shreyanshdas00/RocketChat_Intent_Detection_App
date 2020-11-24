50b1edadface93aa2b270d11d26ccbadb0f900a4
Shreyansh Das <shreyanshdas00@gmail.com>
2020-11-24 11:30:58 +0530

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -10,3 +10,15 @@ mv App.ts DirectContactDetectionApp.ts
 cd ..
 cd rocket.chat
 docker-compose up -d
+read -p "Enter Admin Username: "  username
+read -s -p "Enter Admin Password: " password
+read -p "Enter X-User-Id: "  xuser
+read -p "Enter X-Auth-Token: "  xtoken
+curl -H "X-Auth-Token: $xtoken" \
+     -H "X-User-Id: $xuser" \
+     -H "Content-type: application/json" \
+     http://localhost:3000/api/v1/groups.create \
+     -d '{ "name": "admin" }'
+cd ..
+cd direct-contact-detection
+rc-apps deploy --url http://localhost:3000 --username $username --password $password
