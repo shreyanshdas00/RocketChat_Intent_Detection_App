a1f4eeefe45c1f9630f3524d5140bf5445beaf17
Shreyansh Das <shreyanshdas00@gmail.com>
2020-11-24 16:24:41 +0530

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -10,22 +10,9 @@ mv App.ts DirectContactDetectionApp.ts
 cd ..
 cd rocket.chat
 docker-compose up -d
-sleep 2m
+echo 'Visit your RocketChat URL (http://localhost:3000), setup the server, create private channel "admin" and fill in the following details:' 
 read -p "Enter Admin Username: "  username
-read -p "Enter E-mail: " email
-read -p "Enter Name: " name
 read -p "Enter Admin Password: " password
-curl -H "Content-type:application/json" \
-     http://localhost:3000/api/v1/users.register \
-     -d '{ "username": \"$username\", "email": \"$email\", "pass": \"$password\", "name": \"$name\"}'
-echo "Visit your RocketChat URL (http://localhost:3000), generate access token and fill in the following details:" 
-read -p "Enter X-User-Id: "  xuser
-read -p "Enter X-Auth-Token: "  xtoken
-curl -H "X-Auth-Token: $xtoken" \
-     -H "X-User-Id: $xuser" \
-     -H "Content-type: application/json" \
-     http://localhost:3000/api/v1/groups.create \
-     -d '{ "name": "admin" }'
 cd ..
 cd direct-contact-detection
 meteor rc-apps deploy --url http://localhost:3000 --username $username --password $password
