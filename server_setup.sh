c82eee926fb20c59de522fa5a09605e8ea5a9b35
Shreyansh Das <shreyanshdas00@gmail.com>
2020-11-24 15:42:52 +0530

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -10,12 +10,14 @@ mv App.ts DirectContactDetectionApp.ts
 cd ..
 cd rocket.chat
 docker-compose up -d
-echo "Visit your RocketChat URL (http://localhost:3000), setup the server, generate access token and fill in the following details:" 
 read -p "Enter Admin Username: "  username
+read -p "Enter E-mail: " email
+read -p "Enter Name: " name
 read -p "Enter Admin Password: " password
 curl -H "Content-type:application/json" \
      http://localhost:3000/api/v1/users.register \
-     -d '{ "username": "$username", "email": "shreyanshdas00@gmail.com", "pass": "$password", "name": "Shreyansh Das"}'
+     -d '{ "username": $username, "email": $email, "pass": $password, "name": $name}'
+echo "Visit your RocketChat URL (http://localhost:3000), generate access token and fill in the following details:" 
 read -p "Enter X-User-Id: "  xuser
 read -p "Enter X-Auth-Token: "  xtoken
 curl -H "X-Auth-Token: $xtoken" \
